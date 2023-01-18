30fba5ff09ed48db86d353f2bdb10933579d7d8a
unknown <mahelakamansoori29@gmail.com>
2023-01-18 11:24:01 +0530

    pdf-merger

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const multer = require('multer')
+const upload = multer({ dest : 'uploads/' })
+const port = 3000
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,"template/index.html"))
+})
+app.post('/merge', upload.array('pdfs', 2), function (req, res, next) {
+    console.log(req.files.path)
+    res.send({data: req,files})
+    // req.files is array of `photos` files
+    // req.body will contain the text fields, if there were any
+  })
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
